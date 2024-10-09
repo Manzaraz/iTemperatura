b308f58e337409d1fe76baebb418de58b7bd05b2
Christian Manzaraz <magnusmanz@gmail.com>
2024-10-09 12:56:53 -0300

    Initial Commit

---
+++ iTemperatura/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  iTemperatura
+//
+//  Created by Christian Manzaraz on 09/10/2024.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ iTemperatura/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  iTemperatura
+//
+//  Created by Christian Manzaraz on 09/10/2024.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
